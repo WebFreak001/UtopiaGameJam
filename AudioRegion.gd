c1c34787a4219ef0a698712967e2b02e9eda12a1
Sebastian Dorda <hagish@schattenkind.net>
2024-07-06 00:38:22 +0200

    Water drop audio in caves

---
+++ AudioRegion.gd
@@ -0,0 +1,29 @@
+extends Node3D
+
+@export var streams : Array[AudioStream]
+@export var sleep_min : float = 0
+@export var sleep_max : float = 5
+@export var bus : StringName = &"Master"
+
+var sleep_time : float = 0
+
+func _ready():
+	play_next()
+
+func play_next():
+	var stream = streams[randi() % streams.size()]
+	# print("play next stream", stream)
+	$AudioSource.stream = stream
+	$AudioSource.play()
+	$AudioSource.bus = bus
+
+func _process(delta):
+	var source = $AudioSource
+	# print("tick", source.playing, sleep_time)
+	if source.playing:
+		pass
+	elif sleep_time > 0:
+		sleep_time -= delta
+	else:
+		play_next()
+		sleep_time = randf_range(sleep_min, sleep_max)
