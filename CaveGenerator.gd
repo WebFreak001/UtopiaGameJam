3491cfb2411a9169e21beca4b9376fafde5a9d7e
Utopia PC4 <pc4@utopia>
2024-07-06 00:38:13 +0200

    new cave

--- CaveGenerator.gd
+++ CaveGenerator.gd
@@ -2,7 +2,8 @@ extends CSGPolygon3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var outline = [PackedVector3Array(), PackedVector3Array(), PackedVector3Array(), PackedVector3Array()]
+	var outline = [PackedVector3Array(), PackedVector3Array()]
+	var normals = PackedVector2Array()
 
 	for i in range(0, len(self.polygon)):
 		var v = self.polygon[i]
@@ -10,14 +11,15 @@ func _ready():
 
 		var d = (v - prev).normalized()
 		var nrm = Vector2(-d.y, d.x)
+		normals.append(nrm)
 
 		outline[0].append(Vector3(v.x, v.y, 0))
 		var v2 = v + nrm * (0.3 + 0.3 * randf())
-		outline[1].append(Vector3(v2.x, v2.y, 0.8 + randf() * 0.2))
-		var v3 = v - nrm * (0.3 + 0.3 * randf())
-		outline[2].append(Vector3(v3.x, v3.y, 1.6 + randf() * 0.2))
-		var v4 = v - nrm * 8
-		outline[3].append(Vector3(v4.x, v4.y, 1.9))
+		outline[1].append(Vector3(v2.x, v2.y, 1.1 + randf() * 0.2))
+		#var v3 = v - nrm * (0.3 + 0.3 * randf())
+		#outline[2].append(Vector3(v3.x, v3.y, 1.6 + randf() * 0.2))
+		#var v4 = v2 - nrm * (0.5 + 0.2 * randf())
+		#outline[2].append(Vector3(v4.x, v4.y, 1.9))
 
 	for i in range(0, len(outline) - 1):
 		var walls = MeshInstance3D.new()
@@ -34,4 +36,37 @@ func _ready():
 		arrays[Mesh.ARRAY_VERTEX] = vertices
 
 		walls.mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLE_STRIP, arrays)
+		walls.mesh.surface_set_material(0, self.material_override)
 		add_child(walls)
+
+	var roof = MeshInstance3D.new()
+	roof.mesh = ArrayMesh.new()
+	var vertices = PackedVector3Array()
+	for i in range(0, len(outline[len(outline) - 1])):
+		var v = outline[len(outline) - 1][i]
+		vertices.append(v)
+		vertices.append(get_roof_vert(v - Vector3(normals[i].x, normals[i].y, 0)))
+	vertices.append(vertices[0])
+	vertices.append(vertices[1])
+	
+	var arrays = []
+	arrays.resize(Mesh.ARRAY_MAX)
+	arrays[Mesh.ARRAY_VERTEX] = vertices
+
+	roof.mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLE_STRIP, arrays)
+	roof.mesh.surface_set_material(0, self.material_override)
+	add_child(roof)
+
+func get_roof_vert(v):
+	var closest = v
+	var closest_d = INF
+	for curve in get_children():
+		if curve is Path3D:
+			var lv = curve.to_local(to_global(v))
+			for i in range(0, curve.curve.point_count):
+				var c = curve.curve.get_point_position(i)
+				var d = c.distance_squared_to(lv)
+				if d < closest_d:
+					closest_d = d
+					closest = to_local(curve.to_global(c))
+	return closest
--- Movement.gd
+++ Movement.gd
@@ -4,14 +4,29 @@ func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _process(delta):
-	var debug_input = Input.get_vector("debug_move_left", "debug_move_right", "debug_move_fwd", "debug_move_back")
+	var input_dbg = Input.get_vector("move_left", "move_right", "move_back", "move_fwd")
+	var input_both = $RightHand.get_vector2("primary")
+	var input_move = $LeftHand.get_vector2("primary")
 	
-	var applied = ($XRCamera3D.global_transform.basis * Vector3(debug_input.x, 0, debug_input.y)).normalized()
+	if $LeftHand.get_is_active() and not $RightHand.get_is_active():
+		input_both = $LeftHand.get_vector2("primary")
+		input_move = Vector2.ZERO
+
+	input_move.y += input_both.y
+	var rot = input_both.x
 	
-	self.global_translate(Vector3(applied.x, 0, applied.z) * 0.01)
+	var input = input_move if $RightHand.get_is_active() or $LeftHand.get_is_active() else input_dbg
+	
+	var applied: Vector3 = ($XRCamera3D.global_transform.basis * Vector3(input.x, 0, -input.y)).normalized()
+	
+	if abs(applied.length_squared()) > 0.001 * 0.001:
+		self.global_translate(Vector3(applied.x, 0, applied.z) * 0.03)
+
+	if abs(rot) > 0.001:
+		self.rotate_y(rot * -0.03)
 
 func _input(event):
-	if event is InputEventMouseMotion:
+	if event is InputEventMouseMotion and not $LeftHand.get_is_active() and not $RightHand.get_is_active():
 		self.rotate_y(event.relative.x * -0.001)
 	else:
 		if event.is_action_pressed("ui_cancel"):
