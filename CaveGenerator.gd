e839b684d001d719a288e616598550f24090370a
Utopia PC4 <pc4@utopia>
2024-07-06 03:31:11 +0200

    improve cave generator normals

--- CaveGenerator.gd
+++ CaveGenerator.gd
@@ -21,71 +21,98 @@ func _ready():
 		#var v4 = v2 - nrm * (0.5 + 0.2 * randf())
 		#outline[2].append(Vector3(v4.x, v4.y, 1.9))
 
+	var walls = MeshInstance3D.new()
+	walls.mesh = ArrayMesh.new()
+	var vertices = PackedVector3Array()
+	var normals = PackedVector3Array()
+	var uvs = PackedVector2Array()
+	var indices = PackedInt32Array()
+	
 	var num_outlines = len(outline)
 	for i in range(0, num_outlines - 1):
-		var walls = MeshInstance3D.new()
-		walls.mesh = ArrayMesh.new()
-		var vertices = PackedVector3Array()
-		var normals = PackedVector3Array()
-		var uvs = PackedVector2Array()
 		var num_verts = len(outline[i])
-		for j in range(0, num_verts):
-			vertices.append(outline[i][j])
-			vertices.append(outline[i + 1][j])
-			var nrm = Vector3(-normals2d[j].x, -normals2d[j].y, outline[i][j].z - outline[i + 1][j].z).normalized()
+		for j in range(0, num_verts + 1):
+			var a = outline[i][0] if j == num_verts else outline[i][j]
+			var b = outline[i + 1][0] if j == num_verts else outline[i + 1][j]
+			var c = outline[i][num_verts - 1] if j == 0 else outline[i][j - 1]
+			var d = outline[i + 1][num_verts - 1] if j == 0 else outline[i + 1][j - 1]
+			
+			var u = a - b
+			var v = a - c
+
+			var nrm = Vector3(
+				u.y * v.z - u.z * v.y,
+				u.z * v.x - u.x * v.z,
+				u.x * v.y - u.y * v.x,
+			).normalized()
+
+			var idx = len(vertices)
+			vertices.append(a)
+			vertices.append(b)
+			vertices.append(c)
+			vertices.append(d)
+			normals.append(nrm)
+			normals.append(nrm)
 			normals.append(nrm)
 			normals.append(nrm)
 			uvs.append(Vector2(j / float(num_verts), i / float(num_outlines)))
 			uvs.append(Vector2(j / float(num_verts), (i + 1) / float(num_outlines)))
-		vertices.append(vertices[0])
-		vertices.append(vertices[1])
-		normals.append(normals[0])
-		normals.append(normals[1])
-		uvs.append(Vector2(1.0, uvs[0].y))
-		uvs.append(Vector2(1.0, uvs[1].y))
+			uvs.append(Vector2((j + 1) / float(num_verts), i / float(num_outlines)))
+			uvs.append(Vector2((j + 1) / float(num_verts), (i + 1) / float(num_outlines)))
+			
+			indices.append_array([
+				idx + 2, idx + 1, idx,
+				idx + 2, idx + 3, idx + 1
+			])
+
+	var top = outline[len(outline) - 1]
+	var num_verts = len(top)
+	for i in range(0, num_verts + 1):
+		var a = top[0] if i == num_verts else top[i]
+		var an = normals2d[0 if i == num_verts else i]
+		var b = get_roof_vert(a - Vector3(an.x, an.y, 0) * 4)
+		var c = top[num_verts - 1] if i == 0 else top[i - 1]
+		var cn = normals2d[num_verts - 1 if i == 0 else i - 1]
+		var d = get_roof_vert(c - Vector3(cn.x, cn.y, 0) * 4)
 		
-		var arrays = []
-		arrays.resize(Mesh.ARRAY_MAX)
-		arrays[Mesh.ARRAY_VERTEX] = vertices
-		arrays[Mesh.ARRAY_NORMAL] = normals
-		arrays[Mesh.ARRAY_TEX_UV] = uvs
+		var u = a - b
+		var v = a - c
 
-		walls.mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLE_STRIP, arrays)
-		walls.mesh.surface_set_material(0, self.material_override)
-		add_child(walls)
+		var nrm = Vector3(
+			u.y * v.z - u.z * v.y,
+			u.z * v.x - u.x * v.z,
+			u.x * v.y - u.y * v.x,
+		).normalized()
 
-	var roof = MeshInstance3D.new()
-	roof.mesh = ArrayMesh.new()
-	var vertices = PackedVector3Array()
-	var normals = PackedVector3Array()
-	var uvs = PackedVector2Array()
-	var num_verts = len(outline[len(outline) - 1])
-	for i in range(0, num_verts):
-		var v = outline[len(outline) - 1][i]
-		var r = get_roof_vert(v - Vector3(normals2d[i].x, normals2d[i].y, 0))
-		vertices.append(v)
-		vertices.append(r)
-		var nrm = Vector3(-normals2d[i].x, -normals2d[i].y, v.z - r.z).normalized()
+		var idx = len(vertices)
+		vertices.append(a)
+		vertices.append(b)
+		vertices.append(c)
+		vertices.append(d)
+		normals.append(nrm)
+		normals.append(nrm)
 		normals.append(nrm)
 		normals.append(nrm)
 		uvs.append(Vector2(i / float(num_verts), 0.5))
 		uvs.append(Vector2(i / float(num_verts), 1.0))
-	vertices.append(vertices[0])
-	vertices.append(vertices[1])
-	normals.append(normals[0])
-	normals.append(normals[1])
-	uvs.append(uvs[0])
-	uvs.append(uvs[1])
+		uvs.append(Vector2((i + 1) / float(num_verts), 0.5))
+		uvs.append(Vector2((i + 1) / float(num_verts), 1.0))
+		
+		indices.append_array([
+			idx + 2, idx + 1, idx,
+			idx + 2, idx + 3, idx + 1
+		])
 	
 	var arrays = []
 	arrays.resize(Mesh.ARRAY_MAX)
 	arrays[Mesh.ARRAY_VERTEX] = vertices
 	arrays[Mesh.ARRAY_NORMAL] = normals
 	arrays[Mesh.ARRAY_TEX_UV] = uvs
+	arrays[Mesh.ARRAY_INDEX] = indices
 
-	roof.mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLE_STRIP, arrays)
-	roof.mesh.surface_set_material(0, self.material_override)
-	add_child(roof)
+	walls.mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, arrays)
+	walls.mesh.surface_set_material(0, self.material_override)
+	add_child(walls)
 
 func get_roof_vert(v):
 	var closest = v
